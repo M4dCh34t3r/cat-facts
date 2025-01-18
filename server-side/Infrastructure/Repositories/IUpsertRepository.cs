37d3f648db41f80b31cee5671a38aa82223fdc55
Pedro Andrade <pedroandrade20xx@gmail.com>
2025-01-18 17:04:23 -0300

    chore(server): 'upsert repository' created

---
+++ server-side/Infrastructure/Repositories/IUpsertRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public interface IUpsertRepository
+{
+    /// <summary>
+    /// Inserts and updates multiple facts in the repository based on the provided public fact data.
+    /// </summary>
+    /// <param name="publicFact">The public fact data to upsert.</param>
+    /// <param name="requestURI">The request uri from where the data was fetched.</param>
+    /// <returns>
+    /// A task that represents the asynchronous operation.
+    /// The task result contains the upserted facts.
+    /// </returns>
+    /// <exception cref="DbUpdateException">Thrown if the specified "Text" is conflicting with an already existing entry.</exception>
+    Task<IEnumerable<Fact>> UpsertAsync(PublicFact publicFact, string requestURI);
+}
---
+++ server-side/Infrastructure/Repositories/UpsertRepository.cs
@@ -0,0 +1,54 @@
+﻿using Core.Domain.Entities;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class UpsertRepository(AppDbContext context) : IUpsertRepository
+{
+    private readonly Serilog.ILogger _logger = Serilog.Log.ForContext<UpsertRepository>();
+
+    private readonly AppDbContext _context = context;
+
+    public async Task<IEnumerable<Fact>> UpsertAsync(PublicFact publicFact, string requestURI)
+    {
+        var facts = TransformFact(publicFact, requestURI).ToArray();
+
+        if (facts.Length == 0)
+        {
+            _logger.Debug("No facts to persist.");
+            return facts;
+        }
+
+        var factTexts = facts.Select(f => f.Text).ToHashSet();
+
+        var existingFacts = await _context
+            .Facts.Where(f => factTexts.Contains(f.Text))
+            .ToListAsync();
+
+        foreach (var existingFact in existingFacts)
+        {
+            existingFact.OccurrenceCount++;
+            _context.Facts.Update(existingFact);
+        }
+
+        var newFacts = facts.Where(f => !existingFacts.Any(ef => ef.Text == f.Text)).ToList();
+
+        if (newFacts.Count > 0)
+            await _context.Facts.AddRangeAsync(newFacts);
+
+        var persistedCount = await _context.SaveChangesAsync();
+        _logger.Debug(
+            "Successfully persisted {PersistedCount} fact(s) (updated or inserted) into the database.",
+            persistedCount
+        );
+        return facts;
+    }
+
+    private static IEnumerable<Fact> TransformFact(PublicFact publicFact, string requestURI) =>
+        publicFact.Data.Select(d => new Fact
+        {
+            Source = requestURI,
+            Text = d.Trim(),
+        });
+}
