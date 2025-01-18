9b22f51a5c75600f55ec62f879023a9aea32d19a
Pedro Andrade <pedroandrade20xx@gmail.com>
2025-01-18 17:21:37 -0300

    chore(server): 'APIFact' and 'Paginated' models added to the 'application' layer

---
+++ server-side/Core.Application/Models/APIFact.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Application.Models;
+
+/// <summary>
+/// Represents a cat fact for an API.
+/// </summary>
+public class APIFact
+{
+    /// <summary>
+    /// Gets or sets the unique identifier for the cat fact.
+    /// </summary>
+    [Key]
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the cat fact text.
+    /// </summary>
+    public string Text { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the fact insertion date and time.
+    /// </summary>
+    public DateTime InsertedAt { get; set; }
+
+    /// <summary>
+    /// Gets or sets the fact occurrence count.
+    /// </summary>
+    public int OccurrenceCount { get; set; } = 0;
+
+    /// <summary>
+    /// Gets or sets the fact like count.
+    /// </summary>
+    public int LikeCount { get; set; } = 0;
+
+    /// <summary>
+    /// Gets or sets the fact dislike count.
+    /// </summary>
+    public int DislikeCount { get; set; } = 0;
+}
---
+++ server-side/Core.Application/Models/Paginated.cs
@@ -0,0 +1,33 @@
+﻿namespace Core.Application.Models;
+
+/// <summary>
+/// A paginated enumerable of items.
+/// </summary>
+/// <typeparam name="T">The type of the items in the list.</typeparam>
+public class Paginated<T>(IEnumerable<T> items, int pageIndex, int pageSize, int totalItens)
+{
+    /// <summary>
+    /// The current page items.
+    /// </summary>
+    public IEnumerable<T> Items { get; set; } = items;
+
+    /// <summary>
+    /// The current page index.
+    /// </summary>
+    public int PageIndex { get; set; } = pageIndex;
+
+    /// <summary>
+    /// The current page size.
+    /// </summary>
+    public int PageSize { get; set; } = pageSize;
+
+    /// <summary>
+    /// The total number of items across all pages.
+    /// </summary>
+    public int TotalItems { get; set; } = totalItens;
+
+    /// <summary>
+    /// The total number of pages.
+    /// </summary>
+    public int TotalPages { get; set; } = (int)Math.Ceiling(totalItens / (double)pageSize);
+}
