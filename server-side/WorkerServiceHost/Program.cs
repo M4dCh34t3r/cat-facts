2fac01ca5298519068989a9a800ede9093cfeb83
Pedro Andrade <pedroandrade20xx@gmail.com>
2025-01-18 17:16:08 -0300

    chore(server): implemented upsert job functionality in 'WorkerServiceHost'

--- server-side/WorkerServiceHost/Program.cs
+++ server-side/WorkerServiceHost/Program.cs
@@ -1,11 +1,58 @@
+using BackgroundJobs;
+using Shared.Utils;
+using Hangfire;
 using Infrastructure.Context;
+using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 
-var builder = Host.CreateApplicationBuilder(args);
+string[] settingKeysToCheck =
+[
+    "ConnectionStrings:MSSQL",
+    "Misc:PublicAPIRequestUri",
+];
+WebApplication app = BuildWebApp(args);
+using (var scope = app.Services.CreateScope())
+{
+    var recurringJobManager = scope.ServiceProvider.GetRequiredService<IRecurringJobManager>();
+    recurringJobManager.AddOrUpdate(
+        "DefaultUpsertJob",
+        () => scope.ServiceProvider.GetRequiredService<IUpsertJob>().ExecuteAsync(),
+#if DEBUG
+        Cron.Minutely()
+#else
+        Cron.Hourly()
+#endif
+    );
+}
+StartupUtil.RunApp(app, settingKeysToCheck);
 
-builder.Services.AddDbContext<AppDbContext>(o =>
-    o.UseSqlServer(builder.Configuration.GetConnectionString("MSSQL"))
-);
+static void AddServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddDbContext<AppDbContext>(o =>
+        o.UseSqlServer(builder.Configuration.GetConnectionString("MSSQL"))
+    );
 
-var host = builder.Build();
-host.Run();
+    builder.Services.AddHangfire(o =>
+        o.UseSqlServerStorage(builder.Configuration.GetConnectionString("MSSQL"))
+    );
+    builder.Services.AddHangfireServer();
+
+    builder.Services.AddHttpClient();
+
+    builder
+        .Services.AddScoped<IUpsertJob, UpsertJob>()
+        .AddScoped<IUpsertRepository, UpsertRepository>();
+}
+
+static WebApplication BuildWebApp(string[] args)
+{
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+    StartupUtil.AddSerilog(builder);
+    StartupUtil.AddSentry(builder);
+
+    AddServices(builder);
+
+    return builder.Build();
+}
