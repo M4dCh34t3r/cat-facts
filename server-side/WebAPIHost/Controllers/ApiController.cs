a503f2c0320e0ce36e747adf0b796a8de416c750
Pedro Andrade <pedroandrade20xx@gmail.com>
2025-01-18 17:37:17 -0300

    feat(server): 'ApiController' class implemented

---
+++ server-side/WebAPIHost/Controllers/ApiController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPIHost.Exceptions;
+
+namespace WebAPIHost.Controllers;
+
+[ApiController]
+[Route("/Api/[controller]")]
+public class ApiController() : ControllerBase
+{
+    private readonly Serilog.ILogger _logger = Serilog.Log.ForContext<ApiController>();
+
+    protected IActionResult Execute(Func<IActionResult> func)
+    {
+        try
+        {
+            return func();
+        }
+        catch (ServiceException ex)
+        {
+            _logger.Debug(
+                ex,
+                "Expected \"{name}\" occurred when processing request",
+                nameof(ServiceException)
+            );
+            return ex.ToObjectResult();
+        }
+        catch (Exception ex)
+        {
+            _logger.Error(
+                ex,
+                "Unexpected \"{name}\" occurred when processing request",
+                nameof(Exception)
+            );
+            return StatusCode(500);
+        }
+    }
+
+    protected async Task<IActionResult> ExecuteAsync(Func<Task<IActionResult>> func)
+    {
+        try
+        {
+            return await func();
+        }
+        catch (ServiceException ex)
+        {
+            _logger.Debug(
+                ex,
+                "Expected \"{name}\" occurred when processing request",
+                nameof(ServiceException)
+            );
+            return ex.ToObjectResult();
+        }
+        catch (Exception ex)
+        {
+            _logger.Error(
+                ex,
+                "Unexpected \"{name}\" occurred when processing request",
+                nameof(Exception)
+            );
+            return StatusCode(500);
+        }
+    }
+}
