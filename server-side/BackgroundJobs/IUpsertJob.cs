8f69dbf6b83b28468d414b6b3da372c06bba5f2f
Pedro Andrade <pedroandrade20xx@gmail.com>
2025-01-18 17:10:42 -0300

    chore(server): 'upsert job' added to 'BackgroundJobs'

---
+++ server-side/BackgroundJobs/IUpsertJob.cs
@@ -0,0 +1,12 @@
+﻿namespace BackgroundJobs;
+
+public interface IUpsertJob
+{
+    /// <summary>
+    /// Executes the upsert job, fetching data from an external API and persisting it to the repository.
+    /// </summary>
+    /// <returns>
+    /// A task that represents the asynchronous execution of the upsert job.
+    /// </returns>
+    Task ExecuteAsync();
+}
---
+++ server-side/BackgroundJobs/UpsertJob.cs
@@ -0,0 +1,44 @@
+﻿using Shared.Utils;
+using Core.Domain.Entities;
+using Hangfire;
+using Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+
+namespace BackgroundJobs;
+
+public class UpsertJob(
+    HttpClient httpClient,
+    IConfiguration configuration,
+    IUpsertRepository repository
+) : IUpsertJob
+{
+    private readonly Serilog.ILogger _logger = Serilog.Log.ForContext<UpsertJob>();
+
+    private readonly HttpClient _httpClient = httpClient;
+    private readonly IConfiguration _configuration = configuration;
+    private readonly IUpsertRepository _repository = repository;
+
+    public async Task ExecuteAsync()
+    {
+        try
+        {
+            /// GUARANTEE: The configuration keys are validated via 'StartupUtil.HasMissingConfiguration' in 'Program.cs'
+            string requestUri = _configuration.GetValue<string>("Misc:PublicAPIRequestUri")!;
+
+            var response = await _httpClient.GetAsync(requestUri);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                if (JsonUtil.Deserialize<PublicFact>(json) is PublicFact publicFact)
+                    BackgroundJob.Enqueue(() => _repository.UpsertAsync(publicFact, requestUri));
+            }
+            else
+                _logger.Warning("Failed to fetch data from the API.");
+        }
+        catch (Exception ex)
+        {
+            _logger.Error(ex, "An error occurred while fetching from the API.");
+        }
+    }
+}
