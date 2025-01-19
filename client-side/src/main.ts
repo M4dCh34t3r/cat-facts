ba733abf55a38ba020f0eb1b1bd4aefc902d3f57
Pedro Andrade <pedroandrade20xx@gmail.com>
2025-01-19 11:17:43 -0300

    chore(client): vuetify plugin added

--- client-side/src/main.ts
+++ client-side/src/main.ts
@@ -3,6 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
 import router from './plugins/router'
 import pinia from './plugins/pinia'
 import axios from './plugins/axios'
@@ -12,5 +13,6 @@ const app = createApp(App)
 app.use(axios)
 app.use(pinia)
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
