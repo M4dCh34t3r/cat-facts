bdac8bd8f86c922bec7434b32e343dbd03cc65d5
Pedro Andrade <pedroandrade20xx@gmail.com>
2025-01-19 13:25:59 -0300

    feat(client): 'HomeView' redesigned and 'DivHomeMessages' component added

---
+++ client-side/src/components/divs/DivHomeMessages.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { useConfigStore } from '@/stores';
+import { storeToRefs } from 'pinia';
+import homemessages from '@/assets/homemessages.json';
+
+const { inDarkMode } = storeToRefs(useConfigStore());
+</script>
+
+<template>
+  <div class="justify-center align-center flex-column d-flex ga-4">
+    <v-card
+      class="align-center d-flex"
+      width="calc(100% -  64px)"
+      max-width="640"
+      elevation="8"
+      height="50%"
+      color="mix"
+    >
+      <v-carousel :show-arrows="false" hide-delimiters cycle>
+        <v-carousel-item v-for="homemessage in homemessages" :key="homemessage.id">
+          <div class="justify-center flex-column d-flex h-100">
+            <v-card-title>
+              {{ homemessage.title }}
+            </v-card-title>
+            <strong class="text-medium-emphasis">
+              {{ homemessage.text }}
+            </strong>
+          </div>
+        </v-carousel-item>
+      </v-carousel>
+
+      <v-switch
+        class="position-absolute bottom-0 right-0 mr-4"
+        false-icon="mdi-weather-night"
+        true-icon="mdi-weather-sunny"
+        v-model="inDarkMode"
+      />
+    </v-card>
+    <v-btn text="check facts" class="px-16" color="info" disabled />
+  </div>
+</template>
--- client-side/src/views/HomeView.vue
+++ client-side/src/views/HomeView.vue
@@ -1,5 +1,127 @@
+<script setup lang="ts">
+import { useConfigStore } from '@/stores';
+import { storeToRefs } from 'pinia';
+import { onBeforeUnmount, onMounted, ref } from 'vue';
+import DivHomeMessages from '@/components/divs/DivHomeMessages.vue';
+
+const imgSlideInterval = 5000;
+const imgSlideSrc = [
+  'banners/black-cat.jpg',
+  'banners/calico-cat.jpg',
+  'banners/gray-cat.jpg',
+  'banners/siamese-cat.jpg',
+  'banners/white-cat.jpg',
+  'banners/yellow-cat.jpg'
+];
+
+let intervalId: number | undefined;
+
+const imgSlideSrcIndex = ref<number>(0);
+const { inMobileMode, showAppBar } = storeToRefs(useConfigStore());
+
+function slideshowStart() {
+  intervalId = setInterval(() => {
+    imgSlideSrcIndex.value = (imgSlideSrcIndex.value + 1) % imgSlideSrc.length;
+  }, imgSlideInterval);
+}
+
+function slideshowStop() {
+  if (typeof intervalId === 'number') {
+    clearInterval(intervalId);
+    intervalId = undefined;
+  }
+}
+
+onBeforeUnmount(() => slideshowStop());
+
+onMounted(() => {
+  showAppBar.value = false;
+  slideshowStart();
+});
+</script>
+
 <template>
-  <main>
-    Hello
-  </main>
+  <div class="home-view">
+    <v-card class="crd-title" variant="flat">
+      <img src="/title.svg" width="100%" />
+    </v-card>
+
+    <DivHomeMessages class="div-home-messages" v-if="inMobileMode" height="320" />
+
+    <template v-else>
+      <v-card class="crd-desktop">
+        <Transition>
+          <img :src="imgSlideSrc[imgSlideSrcIndex]" :key="imgSlideSrcIndex" class="img-banner" />
+        </Transition>
+        <DivHomeMessages class="div-home-messages" />
+      </v-card>
+      <b class="text-disabled">
+        The images displayed on the card were AI generated and are for illustrative purposes only.
+      </b>
+    </template>
+  </div>
 </template>
+
+<style scoped>
+.crd-desktop {
+  justify-content: space-evenly;
+  max-width: 120dvh;
+  margin: 64px 0;
+  display: flex;
+  height: 60dvh;
+  width: 100%;
+}
+
+.crd-title {
+  align-items: center;
+  min-width: 320px;
+  display: flex;
+  padding: 8px;
+  width: 50%;
+}
+
+.div-home-messages {
+  position: absolute;
+  height: 100%;
+  width: 50%;
+  right: 0;
+}
+
+.home-view {
+  justify-content: space-evenly;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  display: flex;
+  height: 100%;
+}
+
+.img-banner {
+  position: absolute;
+  height: 60dvh;
+  width: 60dvh;
+  left: 0;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 2.5s ease-in-out;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
+@media (max-width: 1024px) {
+  .crd-title {
+    width: calc(100% - 64px);
+    margin: 32px 0 0;
+  }
+
+  .div-home-messages {
+    position: relative;
+    width: 100%;
+  }
+}
+</style>
