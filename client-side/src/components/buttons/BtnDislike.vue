5207c37acafced2b39aac5618e526e805d3fd42e
Pedro Andrade <pedroandrade20xx@gmail.com>
2025-01-19 13:33:34 -0300

    chore(client): 'Like' and 'Dislike' button components added

---
+++ client-side/src/components/buttons/BtnDislike.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { VIcon } from 'vuetify/components';
+
+const cpnIcon = ref<typeof VIcon>();
+
+const emit = defineEmits<{
+  (e: 'click'): void;
+}>();
+
+function click() {
+  const el = cpnIcon.value?.$el;
+  el.classList.add('animate');
+  el.addEventListener('animationend', () => el.classList.remove('animate'));
+  emit('click');
+}
+
+defineProps<{
+  amount: number;
+  color?: string;
+  disabled?: boolean;
+  location?: 'top' | 'bottom' | 'left' | 'right';
+  text: string;
+}>();
+</script>
+
+<template>
+  <v-tooltip :location="location || 'left'" :text="text">
+    <template #activator="{ props }">
+      <v-badge :content="amount" color="error" offset-x="2" offset-y="2" max="99">
+        <v-btn
+          :color="color || 'grey'"
+          :disabled="disabled"
+          @click="click"
+          size="x-small"
+          v-bind="props"
+          class="ma-1"
+          icon
+        >
+          <v-icon ref="cpnIcon" size="24">{{ 'mdi-thumb-down' }}</v-icon>
+        </v-btn>
+      </v-badge>
+    </template>
+  </v-tooltip>
+</template>
+
+<style scoped>
+.animate {
+  animation: animation 0.5s ease-in-out;
+}
+
+@keyframes animation {
+  0%,
+  100% {
+    transform: rotate(0) scale(1);
+  }
+  33.33% {
+    transform: rotateX(45deg) rotateZ(-45deg) scale(1.5);
+  }
+}
+</style>
