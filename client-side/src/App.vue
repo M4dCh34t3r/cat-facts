8152fc0d0740b862a27ff02b6a10bef8c7f24e6a
Pedro Andrade <pedroandrade20xx@gmail.com>
2025-01-19 11:39:23 -0300

    feat(client): Added 'overlay dialogs' component for use throughout the app

--- client-side/src/App.vue
+++ client-side/src/App.vue
@@ -1,10 +1,27 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import OverlayStoreDialogs from './components/misc/OverlayStoreDialogs.vue';
+import { computed, onMounted } from 'vue';
+import { useConfigStore, useOverlayStore } from './stores';
+import { storeToRefs } from 'pinia';
+
+const { inDarkMode } = storeToRefs(useConfigStore());
+
+const appTheme = computed(() => (inDarkMode.value ? 'defaultDark' : 'defaultLight'));
+
+onMounted(() => useOverlayStore().showInfoMessageDialog('OI', 'Ima info message', 500));
 </script>
 
 <template>
-  <v-app>
+  <v-app :theme="appTheme">
     <v-main>
+      <OverlayStoreDialogs />
+      <v-btn
+        append-icon="mdi-theme-light-dark"
+        @click="inDarkMode = !inDarkMode"
+        text="toggle theme"
+        color="success"
+      />
       <RouterView />
     </v-main>
   </v-app>
